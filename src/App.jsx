6ee8ffe887e7799a9239ce8244b05a44c2060e0f
Guilherme Santos <41321@raphaeldisanto.com.br>
2026-04-16 11:52:53 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,25 @@ import './App.css'
 
 // A função começa aqui
 export default function App() {
-  // O estado tem que estar aqui dentro!
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <main>
-        <h1>Carros</h1>
+        <div className="fundo-gradiente">
+          <h1>Carros</h1>
+          {/* Resto do seu conteúdo */}
+        </div>
+        <div className="seleção de carros" >
+          <div className="selecao-carros">
+  <img 
+    src="https://images.unsplash.com/photo-1503376780353-7e6692767b70?auto=format&fit=crop&q=80&w=800" 
+    alt="Carro Esportivo" 
+    className="imagem-carro"
+  />
+  <h3>Porsche 911</h3>
+  <p>O clássico dos sonhos.</p>
+</div>
+        </div>
       </main>
     </>
   )
 }
-
